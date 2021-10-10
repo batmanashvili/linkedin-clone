32f65a89fc975fb80242bbda46b4b430df0bd8ad
Beka-bat1 <bekabatmanashili1@gmail.com>
2021-10-10 05:43:47 +0400

    feat: project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {registerRootComponent} from 'expo';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import {Text, View, StyleSheet} from 'react-native';
+
+function App() {
+   return (
+      <View style={styles.container}>
+         <Text>Open up App.tsx to start working on your app!</Text>
+         <StatusBar style="auto" />
+      </View>
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+   },
 });
+
+export default registerRootComponent(App);
